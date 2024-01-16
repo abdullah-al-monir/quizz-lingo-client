fdd0cca14213fb4da0be34b71e83a6fa59007350
MrColonThree <abdullahalmonir364@gmail.com>
2024-01-16 23:13:20 +0600

    Setup the project. Installed react, react router, tailwind css. Created some pages without information

---
+++ src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,10 @@
+
+const AboutUs = () => {
+  return (
+    <div>
+      <p>This is about us page</p>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ src/pages/LeaderBoard/LeaderBoard.jsx
@@ -0,0 +1,9 @@
+const LeaderBoard = () => {
+  return (
+    <div>
+      <p>This is leader board page</p>
+    </div>
+  );
+};
+
+export default LeaderBoard;
